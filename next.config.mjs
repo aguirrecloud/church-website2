7e8386f4dc7f2f4eaccd37d27eb3c5cf09b99cdb
aguirrecloud <gabriel@aguirre.cloud>
2025-06-29 13:00:09 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true
+  },
+  basePath: '/aguirre.cloud'
+}
+
+export default nextConfig
